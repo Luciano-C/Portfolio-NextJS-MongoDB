a2d0e65435d4b5948ce4438b57c39da4128e40a5
Luciano <91077377+Luciano-C@users.noreply.github.com>
2022-08-31 16:46:54 -0400

    Error de tipeo en api

--- src/pages/api/technologies/index.js
+++ src/pages/api/technologies/index.js
@@ -7,8 +7,8 @@ export default async function handler(req, res) {
     switch (req.method) {
         case "GET":
             try {
-                const techonologies = await Technology.find().sort({ index: "ascending" });
-                return res.status(200).json(techonologies);
+                const technologies = await Technology.find().sort({ index: "ascending" });
+                return res.status(200).json(technologies);
             } catch (error) {
                 return res.status(500).json({error: error.message});
             }
